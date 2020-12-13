8042ed379d8ed50dca5d962d9db8b9f58154c03b
Ade naufal Abiyyu <psoycs21@gmail.com>
2020-12-13 17:25:43 +0700

    Add files via upload

---
+++ simple-doorLock-16x2 LCD/include/task.h
@@ -0,0 +1,17 @@
+/*
+ * task.h
+ *
+ *  Created on: Dec 12, 2020
+ *      Author: psoycs
+ */
+
+#ifndef INC_TASK_H_
+#define INC_TASK_H_
+#include "stm32f4xx_hal.h"
+void task(void);
+uint8_t b1Read(void);
+uint8_t b2Read(void);
+uint8_t b3Read(void);
+uint8_t b4Read(void);
+
+#endif /* INC_TASK_H_ */
